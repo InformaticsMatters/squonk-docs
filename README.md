55ceb8ba33e903934f74d7464e41b3d1bb95cdad
Oliver Dudgeon <oliverdudgeon@gmail.com>
2019-01-03 20:24:19 +0000

    Improve the build script to include a production option

--- README.md
+++
@@ -1,4 +0,0 @@
-# Base Squonk Docs
-
-Forked to be used by customers and the main squonk documentation page. 
-
--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 if [[ -f "index.md" || -f "_config.yml" || -f "header.md" ]]
 then
-  bundle exec jekyll build
+  if [[ $1 == "production"]]
+  then
+    JEKYLL_ENV=production
+    bundle exec jekyll build
+  else
+    bundle exec jekyll build
+  fi
 else
   echo "index.md, _config.yml, header.md don't exist. Please run init.sh first"
 fi
