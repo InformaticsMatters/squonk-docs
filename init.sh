560814e1171e37e4152ae4d1e0730a677f8b33ff
Oliver Dudgeon <oliverdudgeon@gmail.com>
2019-01-03 19:09:31 +0000

    Mechanism for customer editable content.
    Create two shell scripts that create copies of the
    files that customers should edit. These are sourced from
    the templates directory.

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if ! [[ -f "index.md" ]]
+then
+	cp templates/index-template.md index.md
+fi
+
+if ! [[ -f "header.md" ]]
+then
+	cp templates/header-template.md header.md
+fi
+
+if ! [[ -f "_config-template.md" ]]
+then
+	cp templates/_config-template.yml _config.yml
+fi
